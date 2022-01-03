78723fd322b9f8c34fba11f1dbd448751d7690cb
Zixu Zhang <zixuz@princeton.edu>
2022-01-03 14:59:24 -0500

    work on the setup

--- Jetson_Setup/init.sh
+++ Jetson_Setup/init.sh
@@ -5,7 +5,7 @@ cur_dir=$PWD
 # work_dir 
 work_dir=~/Documents
 
-cd $work_dir
+#cd $work_dir
 sudo apt-get update 
 sudo apt-get -y upgrade
 
@@ -13,11 +13,11 @@ sudo apt-get -y upgrade
 sudo apt install -y git-all
 
 # install python related packages
-sudo apt-get remove python-*
-sudo apt autoremove
+#sudo apt-get remove python-*
+#sudo apt autoremove -y
 
 sudo apt-get install python3.8-dev
-update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.8 1
+#update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.8 1
 
 
 # wget https://bootstrap.pypa.io/pip/2.7/get-pip.py
@@ -25,7 +25,7 @@ update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.8 1
 # rm get-pip*
 
 wget https://bootstrap.pypa.io/get-pip.py
-sudo -H python3 get-pip.py
+sudo -H python3.8 get-pip.py
 rm get-pip*
 
 sudo -H pip3 install matplotlib scipy numpy virtualenv pyyaml
@@ -33,8 +33,8 @@ sudo -H pip3 install matplotlib scipy numpy virtualenv pyyaml
 # install jetson stats
 sudo -H pip3 install -U jetson-stats
 
-sh $cur_dir/install_ros.sh
-sh $cur_dir/install_zed.sh
+# sh $cur_dir/install_ros.sh
+# sh $cur_dir/install_zed.sh
 
 # Install controller dependence
 sudo apt-get install libusb-1.0-0-dev mono-runtime libmono-system-windows-forms4.0-cil -y
@@ -42,7 +42,5 @@ sudo apt-get install libusb-1.0-0-dev mono-runtime libmono-system-windows-forms4
 
 cd $cur_dir
 
-sh install_zed.sh
-sh install_ros.sh
-sh install_acados.sh
+
 
