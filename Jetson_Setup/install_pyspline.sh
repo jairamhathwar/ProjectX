992beecee85d2683e92c3aa65c75b27ff282c344
Divyanshu Pachisia <divyanshupachisia@princeton.edu>
2022-02-14 15:46:16 -0500

    update setup:

--- Jetson_Setup/install_pyspline.sh
+++ Jetson_Setup/install_pyspline.sh
@@ -1,3 +1,21 @@
+#!/bin/bash
+
+
+work_dir=~/Documents
+cd $work_dir
+source ACADOS_env/bin/activate
+
+pip install pyyaml rospkg empy sympy catkin_pkg
+
+pip install spatialmath-python # pyclothoids
+
+# install gfortran
+sudo apt-get install -y gfortran
+# install pyspline
+git clone https://github.com/mdolab/pyspline.git
+cd pyspline
+
+cat << EOF > config/config.mk
 # Config File for LINUX and GFORTRAN Compiler
 AR       = ar
 AR_FLAGS = -rvs
@@ -19,3 +37,7 @@ F2PY = f2py
 # Define additional flags for linking
 LINKER_FLAGS = 
 SO_LINKER_FLAGS =-fPIC -shared
+EOF
+
+make
+pip install .
